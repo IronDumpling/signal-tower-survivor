5ddc8e899564ea63fb597f3efa89aae9a5297cac
Chuyue Zhang <irondumpling233@outlook.com>
2023-09-16 18:22:26 -0400

    ss

--- Assets/Scripts/MainBoard.cs
+++ Assets/Scripts/MainBoard.cs
@@ -7,7 +7,7 @@ public class MainBoard : MonoBehaviour
     List<Device> devices = new List<Device>();
 
     [SerializeField] int GRID_SIZE = 10;
-    [SerializeField] int UNIT_SIZE = 1;
+    [SerializeField] float UNIT_SIZE = 1f;
 
     Grid<Cell> m_CellGrid;
     Grid<Interface> m_XInterfaceGrid;
@@ -17,9 +17,9 @@ public class MainBoard : MonoBehaviour
     {
         m_CellGrid = new Grid<Cell>(GRID_SIZE, GRID_SIZE, UNIT_SIZE,
                                     new Vector3(0, 0, 0));
-        m_XInterfaceGrid = new Grid<Interface>(GRID_SIZE, GRID_SIZE, UNIT_SIZE,
-                                                new Vector3(0, 0, 0));
-        m_YInterfaceGrid = new Grid<Interface>(GRID_SIZE, GRID_SIZE, UNIT_SIZE,
-                                                new Vector3(0, 0, 0));
+        m_XInterfaceGrid = new Grid<Interface>(GRID_SIZE, GRID_SIZE+1, UNIT_SIZE,
+                                                new Vector3(-UNIT_SIZE / 2f, 0, 0));
+        m_YInterfaceGrid = new Grid<Interface>(GRID_SIZE+1, GRID_SIZE, UNIT_SIZE,
+                                                new Vector3(0, UNIT_SIZE / 2f, 0));
     }
 }
